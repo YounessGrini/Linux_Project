621fa99b0e538904c7079483733184239c5c1b66
YounessGrini <100791658+YounessGrini@users.noreply.github.com>
2023-03-27 17:28:26 +0200

    Update eth.sh

--- eth.sh
+++ eth.sh
@@ -3,15 +3,11 @@
 # Fetch the web page HTML
 site=$(curl -s https://www.coingecko.com/en/coins/ethereum)
 
-
 # Extract the Ethereum Price
 price=$(echo "$site" | grep -Po '^\$[\d,]+\.\d{2}$' | tail -n 1)
 
-
-# Get the current time 
-time=$(date +"%Y-%m-%d %H:%M:%S")
-
+# Get the current time
+time=$(date +"%Y-%m-%d-%H:%M:%S")
 
 # Save the result to a file
-echo " $price" >> eth.txt
-echo " $time" >> date.txt
+echo "$price $time" >> eth_data.txt
